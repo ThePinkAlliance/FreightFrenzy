b2f03864c150eda7616ace07f1c21b4a913228f5
MichaelDev11 <beasthunter0114@gmail.com>
2021-11-29 22:16:42 -0500

    added stuff to find the distance of the object.

--- TeamCode/src/main/java/org/firstinspires/ftc/pinkcode/opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/pinkcode/opmodes/Teleop.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.pinkcode.Constants;
 import org.firstinspires.ftc.pinkcode.subsystems.Base;
-import org.firstinspires.ftc.pinkcode.subsystems.Turret;
 import org.firstinspires.ftc.pinkcode.subsystems.Vision;
 import org.firstinspires.ftc.pinkcode.utils.Utils;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -16,25 +15,50 @@ import java.util.List;
 @TeleOp(name = "Teleop", group = "Teleop")
 public class Teleop extends OpMode {
     private Base Base;
-    private Turret Turret;
+    private Vision Vision;
     private Utils Utils;
 
     @Override
     public void init() {
         Base = new Base(hardwareMap);
-        Turret = new Turret(hardwareMap);
-        Utils = new Utils();
+        Vision = new Vision(hardwareMap);
 
+        this.Vision.activate();
         this.Base.configureBase(false);
     }
 
+    //variables used in the following
+    boolean checkedOnce = false;
+    double focalLength = 0;
+    double widthOfObject = 15; //I think it is 15 inches, the object is the shipping hub
     @Override
     public void loop() {
         // drives the left, right side's of the drive train from joystick positions
         this.Base.drive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
 
-        // this will retract the center odometry pod if it gets to close to the barrier
-        this.Base.needToRetract();
+        List<Recognition> recs = Vision.GetAllRecOfLabel("red_shipping");
+
+        //for null statements
+        if (recs != null && !recs.isEmpty()) {
+            Recognition baseRec = recs.get(0);
+            double angleToObject = baseRec.estimateAngleToObject(AngleUnit.DEGREES);
+            double angleToRobot = Utils.math.calculateFromObject(angleToObject, Constants.CAMERA_POSITIONS.FRONT);
+            telemetry.addData("Angle: ", angleToObject);
+            telemetry.addData("Angle From Robot: ", angleToRobot);
+
+            //to find the distance, we need to find the focal length
+            //focal length is easy to find: F = (width of object (px) * distance from object)/widthOfObject
+            if (checkedOnce == false) {
+                int imageWidthPX = baseRec.getImageWidth();
+                int distance = 24; //inches
+                focalLength = (imageWidthPX * distance) / widthOfObject;
+                checkedOnce = true;
+            }
+            //Once we have focal length, the equation for distance(D) is as follows: D = (Width * Focal length)/ new pixel width
+            int newPixelSize = baseRec.getImageWidth();
+            double distanceToObject = (widthOfObject * focalLength) / newPixelSize;
+            telemetry.addData("Distance From Object: ", distanceToObject);
+        }
 
         telemetry.update();
     }
